747739819a14ee644fb9a52350ec107ec1d0b062
leamartinez1707 <leandromart@hotmail.com>
2023-04-19 00:15:17 -0300

    Inicio proyecto

---
+++ src/components/cartWidget/CartWidget.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CartWidget = () => {
+  return (
+    <div className='carrito_nav'>
+      <span>
+      <img src="https://static.vecteezy.com/system/resources/previews/001/504/962/non_2x/shopping-cart-icon-free-vector.jpg" width="50px" alt="" />
+                </span>
+                </div>
+  )
+}
+
+export default CartWidget
+
+
+// rafce
